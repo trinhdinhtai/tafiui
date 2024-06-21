78ed97844a97bc880c7e69b3e8f78c6a3c7725b0
trinhdinhtai <142890841+trinhdinhtai@users.noreply.github.com>
2024-06-21 10:54:53 +0700

    chore: update tailwind config

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="text-red-400">Home</div>;
+  return <div className="text-red-400">Home</div>
 }
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,16 @@
 // tailwind config is required for editor support
 
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import sharedConfig from "@repo/tailwind-config"
+import type { Config } from "tailwindcss"
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   presets: [sharedConfig],
-};
+}
 
-export default config;
+export default config
