44196ea6e32912baccf77054aa1825446d023e3c
trinhdinhtai <142890841+trinhdinhtai@users.noreply.github.com>
2024-06-21 17:54:14 +0700

    chore: update package dependencies and configuration files

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,10 +1,14 @@
 // tailwind config is required for editor support
 
 import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import sharedConfig from "@tafi/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/app/**/*.tsx"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   presets: [sharedConfig],
 };
 
