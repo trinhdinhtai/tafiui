4419c8542fcaa9bfb5f10734b96b29851800cc6d
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2024-07-01 00:05:01 +0700

    feat: add defaultMdxComponents to UI package

---
+++ packages/ui/src/mdx.tsx
@@ -0,0 +1,24 @@
+import { Heading } from "./heading"
+
+const defaultMdxComponents = {
+  h1: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <Heading as="h1" {...props} />
+  ),
+  h2: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <Heading as="h2" {...props} />
+  ),
+  h3: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <Heading as="h3" {...props} />
+  ),
+  h4: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <Heading as="h4" {...props} />
+  ),
+  h5: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <Heading as="h5" {...props} />
+  ),
+  h6: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <Heading as="h6" {...props} />
+  ),
+}
+
+export { defaultMdxComponents as default }
